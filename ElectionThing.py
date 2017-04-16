ce13289a43bb920ec85660f6bb1098bf18572b6b
Wambui <carriekarga@gmail.com>
2017-04-16 15:14:10 +0300

    Hello Message

--- ElectionThing.py
+++ ElectionThing.py
@@ -1,16 +1,22 @@
 from flask import Flask, request
 import json
 import requests
+import tokens
 
 app = Flask(__name__)
 
-# This needs to be filled with the Page Access Token that will be provided
-# by the Facebook App that will be created.
-PAT = 'EAAENedDtJT4BACi4y6Hgh9YJI90jYIDHFvlZBxXFC66ZABnjpfsdU751tmPK6gM0wBm8YoZBDzQZB6DMZBAhTwLtIIZA4kjxyRMZCeJxvKRzZBAsawSOZCZCN4xCuiUJX7ZAx24s5LBr9zrPSY8a7iiDvD91oJNCZBf4UontI3m6eth4ZCAZDZD'
+PAT = tokens.PAT
+VerifyToken = tokens.token
+
+HelloMessage = """
+Hello!
+My name is ElectionBot, and I provide you with information about the 2017 National Elections candidates.
+"""
+
 
 @app.route('/', methods=['GET'])
-def handle_verification():
-  if request.args.get('hub.verify_token', '') == 'test-token':
+def verification():
+  if request.args.get('hub.verify_token', '') == VerifyToken:
     print("Verification successful!")
     return request.args.get('hub.challenge', '')
   else:
@@ -18,40 +24,39 @@ def handle_verification():
     return 'Error, wrong validation token'
 
 @app.route('/', methods=['POST'])
-def handle_messages():
-  payload = request.get_data()
-  print(payload)
-  for sender, message in messaging_events(payload):
-    print("Incoming from %s: %s" % (sender, message))
-    send_message(PAT, sender, message)
-  return "ok"
+def GetMessages():
+  messages = request.get_json
+  for message in messages['entry']:
+  	for msg in message['messaging']:
+  		SenderID = msg['sender']['id']
+  		MessageText = msg['message']['text']
+  		RecipientID = msg['recipient']['id']
+  		if msg.get('message'):
+  			if MessageText.lower() == 'hi' or MessageText.lower() = 'Hello':
+  				SendMessage(SenderID, HelloMessage)
 
-def messaging_events(payload):
-  """Generate tuples of (sender_id, message_text) from the
-  provided payload.
-  """
-  data = json.loads(payload)
-  messaging_events = data["entry"][0]["messaging"]
-  for event in messaging_events:
-    if "message" in event and "text" in event["message"]:
-      yield event["sender"]["id"], event["message"]["text"].encode('unicode_escape')
-    else:
-      yield event["sender"]["id"], "I can't echo this"
+  return 'ok', 200
 
 
-def send_message(token, recipient, text):
+def SendMessage(VerifyToken, RecipientID, Text):
   """Send the message text to recipient with id recipient.
   """
 
-  r = requests.post("https://graph.facebook.com/v2.6/me/messages",
-    params={"access_token": token},
-    data=json.dumps({
-      "recipient": {"id": recipient},
-      "message": {"text": text.decode('unicode_escape')}
-    }),
-    headers={'Content-type': 'application/json'})
-  if r.status_code != requests.codes.ok:
-    print(r.text)
+  parameters = {
+  	'access-token' : 'VerifyToken'
+  }
+  headers = {
+  	'Content-Type' : 'application/json'
+  }
+  data = json.dumps({
+  	'recipient': {
+  		'id': 'RecipientID'
+  	},
+  	'message' : {
+  		'text': 'message_text'
+  	}
+  	})
+  r = requests.post('https://graph.facebook.com/v2.6/me/messages', params = parameters, headers = headers, data = data)
 
 if __name__ == '__main__':
-  app.run()
+  app.run(debug = True)
