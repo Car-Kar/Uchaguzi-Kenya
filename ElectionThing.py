3449e2279122d87c13e88158c4fc2ece613b8537
Wambui <carriekarga@gmail.com>
2017-04-16 12:16:09 +0300

    Paranthesis

--- ElectionThing.py
+++ ElectionThing.py
@@ -20,7 +20,7 @@ def handle_verification():
 @app.route('/', methods=['POST'])
 def handle_messages():
   payload = request.get_data()
-  print payload
+  print(payload)
   for sender, message in messaging_events(payload):
     print("Incoming from %s: %s" % (sender, message))
     send_message(PAT, sender, message)
@@ -51,7 +51,7 @@ def send_message(token, recipient, text):
     }),
     headers={'Content-type': 'application/json'})
   if r.status_code != requests.codes.ok:
-    print r.text
+    print(r.text)
 
 if __name__ == '__main__':
   app.run()
