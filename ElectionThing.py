9ad8ba1d4bbc00c36d02ff54831e654ce4b2f7c8
Wambui <carriekarga@gmail.com>
2017-04-16 16:59:11 +0300

    Test

--- ElectionThing.py
+++ ElectionThing.py
@@ -54,7 +54,7 @@ def SendMessage(RecipientID, Text):
         'text': Text
     }
     })
-    r = requests.post('https://graph.facebook.com/v2.8/me/messages/?access_token' + PAT,  headers=headers, data=data)
+    r = requests.post('https://graph.facebook.com/v2.8/me/messages/?access_token=' + PAT,  headers=headers, data=data)
     if r.status_code != 200:
         print(r.text)
 
