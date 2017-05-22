a1c00a518567439398833a170118eb81a87d57f3
Wambui <carriekarga@gmail.com>
2017-05-22 18:55:23 +0300

    Mentor.

--- Uchaguzi.py
+++ Uchaguzi.py
@@ -78,10 +78,6 @@ def GetMessages():
         for msg in message['messaging']:
             SenderID = msg['sender']['id']
             if msg.get('message'):
-                PostbackText = msg['postback']['payload']
-                if PostbackText == 'Get Started':
-                    SendMessage(SenderID, June)
-                """i = 0
                 MessageText = msg['message']['text']
                 if 'start' in MessageText.lower() or 'hello' in MessageText.lower() or 'hi' in MessageText.lower() or 'hey' in MessageText.lower():
                     SendMessage(SenderID, IntroductoryMessage)
@@ -212,7 +208,6 @@ def GetMessages():
                     SendMessage(SenderID, 'What level of elections do you want? You can choose one from the menu below.')
                 else: 
                     SendMessage(SenderID, ApologyMessage)
-                
             elif msg.get('postback'):
                 i = 0
                 PostbackText = msg['postback']['payload']
@@ -278,7 +273,7 @@ def GetMessages():
                     SendMessage(SenderID, VoterRequirements)
                     SendMessage(SenderID, ContinueUsing)
                 else:
-                    SendMessage(SenderID, ApologyMessage)"""
+                    SendMessage(SenderID, ApologyMessage)
 
   return 'ok', 200
 
