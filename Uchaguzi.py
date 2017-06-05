033bfb26fc272f70ebfdcbeaa7180f716ea83385
Wambui <carriekarga@gmail.com>
2017-06-05 21:24:52 +0300

    Monday

--- Uchaguzi.py
+++ Uchaguzi.py
@@ -739,7 +739,7 @@ If you want to know about another candidate, send me his or her name, otherwise 
                                 SendMessage(SenderID, 'What county are you from?')
                                 CountyOptions(SenderID, 'Choose one below')
 
-                            elif Kiswahili is not True and level = 'survey':
+                            elif Kiswahili is not True and level == 'survey':
                                 SendMessage(SenderID, 'Please answer the followinf few questions.')
                                 ReusableOptions(SenderID, 'Are you familiar with your county administration?', 'Yes', 'No')
 
