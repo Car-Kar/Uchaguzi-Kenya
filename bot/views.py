bfb47f1509e640da85f116c759ca7742a0332766
Wambui Karuga <wambui@karuga.xyz>
2019-03-17 16:12:55 +0300

    Refactor Flask App.

--- bot/views.py
+++ bot/views.py
@@ -6,18 +6,20 @@ from . import bot
 
 
 
-PAT = 'EAAarLkMVMy4BAMKvoGqRqod35PTLNIh6NHjVjKBOiqL3jnVAjBXfwPvXpA5WMJQsstY3VePK8J7jEuGt7ifXC8R0QwvYs05oGRk4JnghiIB2xmZCbsDoLphimTk6vEwe4aYCYTlpBFxIpbZBVSdJGZCl4ZCELZBLiHzhYFJDipQZDZD'
-
+PAT = os.environ.get('PAT', None)
 verify_token = os.environ.get('VERIFY_TOKEN', None)
 
 @bot.route('/', methods=['GET'])
 def verification():
-  if request.args.get('hub.verify_token', '') == verify_token:
-    print("Verification successful!")
-    return request.args.get('hub.challenge', '')
-  else:
-    print("Verification failed!")
-    return 'Error, wrong validation token'
+    if PAT is not None and verify_token is not None:
+        if request.args.get('hub.verify_token', '') == verify_token:
+            print("Verification successful!")
+            return request.args.get('hub.challenge', '')
+        else:
+            print("Verification failed!")
+            return 'Error, wrong validation token'
+    else
+        return "Could not get verification tokens."
 
 
 @bot.route('/', methods=['POST'])
