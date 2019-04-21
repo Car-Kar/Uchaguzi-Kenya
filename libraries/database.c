49dcbd20b377b0a2232e3be0ab1376442e9215f4
Wambui Karuga <wambui@karuga.xyz>
2019-04-21 05:15:22 +0300

    Create database workers.

--- libraries/database.c
+++ libraries/database.c
@@ -1,99 +1,61 @@
-#include <mongoc/mongoc.h>
+#include <database.h>
+#include <stdlib.h>
+#include <stdio.h>
 
-int db_connect()
-{
-   const char *uri_string = "mongodb://localhost:27017";
-   mongoc_uri_t *uri;
-   mongoc_client_t *client;
-   mongoc_database_t *database;
-   mongoc_collection_t *collection;
-   bson_t *command, reply, *insert;
-   bson_error_t error;
-   char *str;
-   bool retval;
-
-   /*
-    * Required to initialize libmongoc's internals
-    */
-   mongoc_init ();
-
-   /*
-    * Optionally get MongoDB URI from command line
-    */
-
-   /*
-    * Safely create a MongoDB URI object from the given string
-    */
-   uri = mongoc_uri_new_with_error (uri_string, &error);
-   if (!uri) {
-      fprintf (stderr,
-               "failed to parse URI: %s\n"
-               "error message:       %s\n",
-               uri_string,
-               error.message);
-      return EXIT_FAILURE;
-   }
-
-   /*
-    * Create a new client instance
-    */
-   client = mongoc_client_new_from_uri (uri);
-   if (!client) {
-      return EXIT_FAILURE;
-   }
-
-   /*
-    * Register the application name so we can track it in the profile logs
-    * on the server. This can also be done from the URI (see other examples).
-    */
-   mongoc_client_set_appname (client, "connect-example");
-
-   /*
-    * Get a handle on the database "db_name" and collection "coll_name"
-    */
-   database = mongoc_client_get_database (client, "db_name");
-   collection = mongoc_client_get_collection (client, "db_name", "coll_name");
-
-   /*
-    * Do work. This example pings the database, prints the result as JSON and
-    * performs an insert
-    */
-   command = BCON_NEW ("ping", BCON_INT32 (1));
-
-   retval = mongoc_client_command_simple (
-      client, "admin", command, NULL, &reply, &error);
-
-   if (!retval) {
-      fprintf (stderr, "%s\n", error.message);
-      return EXIT_FAILURE;
-   }
-
-   str = bson_as_json (&reply, NULL);
-   printf ("%s\n", str);
-
-   insert = BCON_NEW ("hello", BCON_UTF8 ("world"));
-
-   if (!mongoc_collection_insert_one (collection, insert, NULL, NULL, &error)) {
-      fprintf (stderr, "%s\n", error.message);
-   }
-
-   bson_destroy (insert);
-   bson_destroy (&reply);
-   bson_destroy (command);
-   bson_free (str);
-
-   /*
-    * Release our handles and clean up libmongoc
-    */
-   mongoc_collection_destroy (collection);
-   mongoc_database_destroy (database);
-   mongoc_uri_destroy (uri);
-   mongoc_client_destroy (client);
-   mongoc_cleanup ();
-
-   return EXIT_SUCCESS;
-}
 void interface_init() {
    mongoc_init();
+   mongoc_log_set_handler(interface_logger, NULL);
 }
-mongoc_client_pool_t *get_interface_pool();
+
+void interface_cleanup(mongoc_uri_t *uri, mongoc_client_pool_t *pool) {
+   mongoc_client_pool_destroy(pool);
+   mongoc_uri_destroy(uri);
+   mongoc_cleanup();
+}
+
+void interface_logger(mongoc_log_level_t level, const char *domain, const char *message, void *data) {
+   if (level < MONGOC_LOG_LEVEL_INFO) {
+      mongoc_log_default_handler(level, domain, message, data);
+   }
+}
+
+mongoc_client_pool_t *get_interface_pool() {
+   mongoc_uri_t *uri;
+   bson_error_t err;
+   mongoc_client_pool_t *pool = NULL;
+
+   const char *path = getenv("DB_PATH");
+
+   uri = mongoc_uri_new_with_error(path, &err);
+
+   if (!uri) {
+      MONGOC_ERROR("Failed to parse URI string %s: %s\n", path, err.message);
+      interface_cleanup(uri, pool);
+      return NULL;
+   }
+
+   pool = mongoc_client_pool_new(uri);
+
+   if (!pool) {
+      interface_cleanup(uri, pool);
+      return NULL;
+   }
+   mongoc_client_pool_set_error_api (pool, 2);
+   interface_logger(MONGOC_LOG_LEVEL_DEBUG, "pool", "Successfully created interface pool!", NULL);
+   
+   return pool;
+}
+
+void worker_create_collection(void *data) {
+   mongoc_client_t *client;
+
+   mongoc_client_pool_t *pool = data;
+
+   client = mongoc_client_pool_pop();
+
+   if (!client) {
+      interface_logger
+   }
+
+   mongoc_client_pool_push(pool, client);
+}
--- libraries/database.h
+++ libraries/database.h
@@ -3,6 +3,13 @@
 
 #include <mongoc/mongoc.h>
 
+
 void interface_init();
+void interface_cleanup(mongoc_uri_t *, mongoc_client_pool_t *);
+void interface_logger(mongoc_log_level_t level, const char *domain, const char *message, void *data);
 mongoc_client_pool_t  *get_interface_pool();
+void worker_create_collection(void *);
+/*void worker_get_document(void *data);
+void worker_add_document(void *data); */
+
 #endif
